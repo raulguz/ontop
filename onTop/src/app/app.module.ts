914e590672e4676718c8fcac61f4bcd6b6f61ef9
Raul Guzman <raul.guzman@trexis.net>
2022-08-09 23:58:51 -0600

    Implement Search and filter component

--- onTop/src/app/app.module.ts
+++ onTop/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchFilterComponent } from './search-filter/search-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchFilterComponent
   ],
   imports: [
     BrowserModule,
